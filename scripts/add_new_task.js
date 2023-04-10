f15208161845dd9c6e4f5ce32ff9428d0069827d
XavierGong <xavier@XavierGongs-MacBook-Pro.local>
2023-04-10 12:58:09 -0400

    starter

--- scripts/add_new_task.js
+++ scripts/add_new_task.js
@@ -1,9 +1,58 @@
 $(document).ready( function() {
     console.log("Ready!");
-    document.getElementById("newTask").addEventListener("click", redirectToMain);
-
-
+    document.getElementById("newTask").addEventListener("click", storeTask);
+    // document.getElementById("newTask").addEventListener("click", redirectToMain);
 });
+
+
+function storeTask() {
+    let id = parseInt(localStorage.getItem("prev_id")) + 1;
+    localStorage.setItem("prev_id", id);
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+    let status = document.getElementById("status").value;
+    let due = document.getElementById("due-date").value;
+    
+    event.preventDefault();
+    let task_obj = {
+        id: id,
+        title: title,
+        description: description,
+        status: status,
+        due: due
+    };
+
+    // localStorage.setItem(id, JSON.stringify(task_obj));
+
+    // const task = {
+    //     id = id,
+    //     title: document.getElementById('title').value,
+    //     description: document.getElementById('description').value,
+    //     subtasks: document.getElementById('subtasks').value,
+    //     status: document.getElementById('status').value
+    // };
+
+    // let TODO = JSON.parse(localStorage.getItem('TODO'));
+    // let DOING = JSON.parse(localStorage.getItem('DOING'));
+    // let DONE = JSON.parse(localStorage.getItem('DONE'));
+
+    // if(task_obj.status === "TODO") {
+    //     TODO.push(task_obj);
+    //     localStorage.setItem('TODO', JSON.stringify(TODO));
+    //     }
+    // else if (task_obj.status === "DOING") {
+    //     DOING.push(task_obj);
+    //     localStorage.setItem('DOING', JSON.stringify(DOING));
+    // }
+    // else { // DONE
+    //     DONE.push(task_obj);
+    //     localStorage.setItem('DONE', JSON.stringify(DONE));
+    // }
+
+    // form.reset();
+    
+
+}
 
 
 function redirectToMain() {
--- scripts/main_page.js
+++ scripts/main_page.js
@@ -1,11 +1,36 @@
 // Main
 $(document).ready( function() {
+    newBoardButton = $('#add_new_board');
     console.log("Ready!");
+    // Initialize
+    if (localStorage.getItem("prev_id") === null) {
+        localStorage.setItem("prev_id", 0);
+        // localStorage.setItem("todo_id", 0);
+        // localStorage.setItem("doing_id", 0);
+        // localStorage.setItem("done_id", 0);
+        localStorage.setItem('TODO', []);
+        localStorage.setItem('DOING', []);
+        localStorage.setItem('DONE', []);
+    } else {
+        console.log(localStorage.getItem("prev_id"));
+        console.log(localStorage.getItem("TODO"));
+    }
+    
     document.getElementById("add_new_task_text").addEventListener("click", redirectToAddTask);
-
+    newBoardButton.click(CreateNewBoard)
 
 });
 
+
+function CreateNewBoard() {
+    all_boards = localStorage.getItem('all_boards');
+    if (all_boards === null) {
+        var defaultValue = {};
+        localStorage.setItem('all_boards', JSON.stringify(defaultValue));
+    }
+    all_boards = JSON.parse(localStorage.getItem('all_boards'));
+    
+}
 
 function redirectToAddTask() {
     window.location.href = "addNewTask_page.html";
