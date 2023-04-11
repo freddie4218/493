c82687930ded2a7009e24b8b3ecf858a83147098
XavierGong <xavier@0587673473.vpn.umich.net>
2023-04-11 11:34:03 -0400

    main page & add task completed

--- scripts/add_new_task.js
+++ scripts/add_new_task.js
@@ -6,12 +6,17 @@ $(document).ready( function() {
 
 
 function storeTask() {
-    let id = parseInt(localStorage.getItem("prev_id")) + 1;
-    localStorage.setItem("prev_id", id);
     let title = document.getElementById("title").value;
+    if (title.trim() === '') {
+        alert('Please enter a value for title');
+        return
+      }
     let description = document.getElementById("description").value;
     let status = document.getElementById("status").value;
     let due = document.getElementById("due-date").value;
+
+    let id = parseInt(localStorage.getItem("prev_id")) + 1;
+    localStorage.setItem("prev_id", id);
     
     event.preventDefault();
     let task_obj = {
@@ -22,39 +27,28 @@ function storeTask() {
         due: due
     };
 
-    // localStorage.setItem(id, JSON.stringify(task_obj));
+    localStorage.setItem(id, JSON.stringify(task_obj));
 
-    // const task = {
-    //     id = id,
-    //     title: document.getElementById('title').value,
-    //     description: document.getElementById('description').value,
-    //     subtasks: document.getElementById('subtasks').value,
-    //     status: document.getElementById('status').value
-    // };
+    if(task_obj.status === "TODO") {
+        let TODO = JSON.parse(localStorage.getItem('TODO'));
+        TODO.push(task_obj);
+        localStorage.setItem('TODO', JSON.stringify(TODO));
+        }
+    else if (task_obj.status === "DOING") {
+        let DOING = JSON.parse(localStorage.getItem('DOING'));
+        DOING.push(task_obj);
+        localStorage.setItem('DOING', JSON.stringify(DOING));
+    }
+    else { // DONE
+        let DONE = JSON.parse(localStorage.getItem('DONE'));
+        DONE.push(task_obj);
+        localStorage.setItem('DONE', JSON.stringify(DONE));
+    }
 
-    // let TODO = JSON.parse(localStorage.getItem('TODO'));
-    // let DOING = JSON.parse(localStorage.getItem('DOING'));
-    // let DONE = JSON.parse(localStorage.getItem('DONE'));
-
-    // if(task_obj.status === "TODO") {
-    //     TODO.push(task_obj);
-    //     localStorage.setItem('TODO', JSON.stringify(TODO));
-    //     }
-    // else if (task_obj.status === "DOING") {
-    //     DOING.push(task_obj);
-    //     localStorage.setItem('DOING', JSON.stringify(DOING));
-    // }
-    // else { // DONE
-    //     DONE.push(task_obj);
-    //     localStorage.setItem('DONE', JSON.stringify(DONE));
-    // }
-
-    // form.reset();
-    
-
-}
-
-
-function redirectToMain() {
     window.location.href = "main_page.html";
 }
+
+
+// function redirectToMain() {
+//     window.location.href = "main_page.html";
+// }
--- scripts/main_page.js
+++ scripts/main_page.js
@@ -5,15 +5,43 @@ $(document).ready( function() {
     // Initialize
     if (localStorage.getItem("prev_id") === null) {
         localStorage.setItem("prev_id", 0);
-        // localStorage.setItem("todo_id", 0);
-        // localStorage.setItem("doing_id", 0);
-        // localStorage.setItem("done_id", 0);
-        localStorage.setItem('TODO', []);
-        localStorage.setItem('DOING', []);
-        localStorage.setItem('DONE', []);
+        let empty_list = [];
+        localStorage.setItem('TODO', JSON.stringify(empty_list));
+        localStorage.setItem('DOING', JSON.stringify(empty_list));
+        localStorage.setItem('DONE', JSON.stringify(empty_list));
     } else {
         console.log(localStorage.getItem("prev_id"));
-        console.log(localStorage.getItem("TODO"));
+        // Render main page
+        // First render the TODO column
+        let TODO_col = $('#TODO');
+        document.getElementById("TODO_num").innerHTML = "TODO (" + JSON.parse(localStorage.getItem('TODO')).length + ")";
+        for (const task of JSON.parse(localStorage.getItem('TODO'))) {
+            let title = task.title;
+            let description = task.description;
+            let status = task.status;
+            console.log(title);
+            TODO_col.append("<div class='task'>" + title + "<br><div class='subtask'>0 of 2 subtasks done</div></div>")
+        }
+        // Then render the DOING column
+        let DOING_col = $('#DOING');
+        document.getElementById("DOING_num").innerHTML = "DOING (" + JSON.parse(localStorage.getItem('DOING')).length + ")";
+        for (const task of JSON.parse(localStorage.getItem('DOING'))) {
+            let title = task.title;
+            let description = task.description;
+            let status = task.status;
+            console.log(title);
+            DOING_col.append("<div class='task'>" + title + "<br><div class='subtask'>0 of 2 subtasks done</div></div>")
+        }
+        // Finally render the DONE column
+        let DONE_col = $('#DONE');
+        document.getElementById("DONE_num").innerHTML = "DONE (" + JSON.parse(localStorage.getItem('DONE')).length + ")";
+        for (const task of JSON.parse(localStorage.getItem('DONE'))) {
+            let title = task.title;
+            let description = task.description;
+            let status = task.status;
+            console.log(title);
+            DONE_col.append("<div class='task'>" + title + "<br><div class='subtask'>0 of 2 subtasks done</div></div>")
+        }
     }
     
     document.getElementById("add_new_task_text").addEventListener("click", redirectToAddTask);
